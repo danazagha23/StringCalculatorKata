150d2b173292858080d3efea7d0eadb10e6f4b1f
Dana Zagha <dana.zagha0@gmail.com>
2023-04-12 01:30:15 +0300

    step2 branch

--- Program.cs
+++ Program.cs
@@ -10,11 +10,6 @@ namespace StringCalculatorKata
             Console.WriteLine("Enter string contains from 0 to 2 numbers: ");
             string numbers = Console.ReadLine();
 
-            if(numbers.Count(c => c == ',') > 1)
-            {
-                throw new Exception("string must has 2 numbers maximum");
-            }
-
             var sc = new StringCalculatorService();
             Console.WriteLine(sc.Add(numbers));
         }
