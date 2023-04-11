9fd07537cabf4570a85c1bcb66c624a6b8da2b2d
Dana Zagha <dana.zagha0@gmail.com>
2023-04-12 00:39:13 +0300

    step1 branch

--- Program.cs
+++ Program.cs
@@ -1,2 +1,22 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using System;
+
+
+namespace StringCalculatorKata
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            Console.WriteLine("Enter string contains from 0 to 2 numbers: ");
+            string numbers = Console.ReadLine();
+
+            if(numbers.Count(c => c == ',') > 1)
+            {
+                throw new Exception("string must has 2 numbers maximum");
+            }
+
+            var sc = new StringCalculatorService();
+            Console.WriteLine(sc.Add(numbers));
+        }
+    }
+}
