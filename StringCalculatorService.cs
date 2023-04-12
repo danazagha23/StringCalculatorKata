58dff6e8f208370e67b35cb4cc029256ce81f16d
Dana Zagha <dana.zagha0@gmail.com>
2023-04-12 03:13:24 +0300

    step4 branch

--- StringCalculatorService.cs
+++ StringCalculatorService.cs
@@ -8,19 +8,23 @@ namespace StringCalculatorKata
 {
     public class StringCalculatorService
     {
+        public char[] Delimiter { get; set; } = {',', '\n'}; 
         public int Add(string numbers)
         {
             if (numbers.Length == 0) return 0;
 
-            string[] ints = numbers.Split(new char[] { ',', '\n' });
-
+            if(numbers.Substring(0, 2) == "//")
+            {
+                Delimiter = new char[] { numbers[2] };
+                numbers = numbers.Substring(4);
+            }
+            string[] ints = numbers.Split(Delimiter);
+            
             int sum = 0;
-
             foreach (string s in ints)
             {
                 sum += Convert.ToInt32(s);
             }
-
             return sum;
         }
     }
